0fb1a3f190ce8c0f5763307580823672173688ce
devfardin <contactfardin22@gmail.com>
2025-02-03 00:28:40 +0600

    set metadata

---
+++ src/app/(MainLayout)/contact/page.js
@@ -0,0 +1,15 @@
+import React from 'react'
+export const metadata = {
+  title: 'Contact us',
+  description: 'This is contact us page'
+}
+
+const ContactPage = () => {
+  return (
+    <div className='container mx-auto mt-10'>
+      <h1 className='text-2xl font-bold text-green-500'>Contact Us page</h1>
+    </div>
+  )
+}
+
+export default ContactPage
--- src/app/(MainLayout)/page.js
+++ src/app/(MainLayout)/page.js
@@ -1,9 +1,13 @@
 import React from 'react'
+export const metadata = {
+  title: 'Home page',
+  description: 'This is home page'
+}
 
 export default function HomePage() {
   return (
-    <div>
-      <h1 className='text-4xl font-bold'>Hello World</h1>
+    <div className='container mx-auto mt-10'>
+      <h1 className='text-2xl font-bold text-green-500'>Hello World</h1>
     </div>
   )
 }
