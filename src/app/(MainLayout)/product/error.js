56c6bda6f7ef7816952d930df218b90a0073c89d
devfardin <contactfardin22@gmail.com>
2025-02-07 15:57:49 +0600

    error bounder

--- src/app/(MainLayout)/product/error.js
+++ src/app/(MainLayout)/product/error.js
@@ -1,8 +1,14 @@
 "use client"
 const errorPage = ({error, reset}) => {
+  // show error in console
+  console.log(error.message);
+  
   return (
-    <div> <h1 className='text-center text-red-500 my-5 text-xl'> Something went wrong</h1>
-      
+    <div> 
+      <h1 className='text-center text-red-500 my-5 text-xl'> 
+    Something went wrong</h1>
+    
+    <button onClick={() => reset()} className="text-black text-lg font-medium py-2 px-3 rounded border-none">Try Again </button> 
     </div>
   )
 }
