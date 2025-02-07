365de11e11452efe462aa8dff0f893de5f2d6f99
devfardin <contactfardin22@gmail.com>
2025-02-07 15:44:55 +0600

    solve supperss hydration warning

---
+++ src/app/(MainLayout)/product/error.js
@@ -0,0 +1,10 @@
+"use client"
+const errorPage = ({error, reset}) => {
+  return (
+    <div> <h1 className='text-center text-red-500 my-5 text-xl'> Something went wrong</h1>
+      
+    </div>
+  )
+}
+
+export default errorPage
---
+++ src/app/(MainLayout)/product/loading.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const loadingPage = () => {
+  return (
+    <div>
+      <h1 className='text-xl font-medium my-5 text-blue-500'>Loading page.......</h1>
+    </div>
+  )
+}
+
+export default loadingPage
